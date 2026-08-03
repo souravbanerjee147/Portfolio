f37f4297fa3cc4c5adf03a1a8a1ca79b419a7ca4
souravbanerjee147 <souravbanerjeeweb18@gmail.com>
2026-08-03 23:16:46 +0530

    ....

--- backend/controllers/contact.Controller.js
+++ backend/controllers/contact.Controller.js
@@ -14,6 +14,8 @@ export async function sendContactEmail(req, res) {
         // Initialize transporter dynamically so process.env values are fully loaded
         const transporter = nodemailer.createTransport({
             service: 'gmail',
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
